85565d8e9fb3b2d51d0bcb93eb0da20d845d5bab
unknown <mohamedmanef.benmansour@insat.ucar.tn>
2023-12-10 21:35:33 +0100

    gitignore file + signup + signin + jwt

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetNET.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ProjetNET.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private readonly IConfiguration _config;
+
+        public AuthController(AppDbContext db, IConfiguration config)
+        {
+            _db = db;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(User request)
+        {
+            // Check if the request object is null
+            if (request == null)
+            {
+                return BadRequest("Invalid request data");
+            }
+
+            // Validate required fields
+            if (string.IsNullOrEmpty(request.LastName) ||
+                string.IsNullOrEmpty(request.FirstName) ||
+                string.IsNullOrEmpty(request.Email) ||
+                string.IsNullOrEmpty(request.PasswordHash) ||
+                string.IsNullOrEmpty(request.PhoneNumber) || string.IsNullOrEmpty(request.Adress) ||
+                (request.IsAdmin == null))
+            {
+                return BadRequest("LastName, FirstName, Email, PasswordHash, Phone Number, Adress, and IsAdmin are required fields");
+            }
+
+            // Validate email format
+            if (!IsValidEmail(request.Email))
+            {
+                return BadRequest("Invalid email format");
+            }
+            if (_db.Users.Any(u => u.Email == request.Email))
+            {
+                return BadRequest("Email already exists");
+            }
+
+            // Validate password length or other criteria if needed
+            if (request.PasswordHash.Length < 6)
+            {
+                return BadRequest("Password should be at least 6 characters long");
+            }
+
+            // Hash the password
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.PasswordHash);
+
+            // Create a new user instance
+            var user = new User
+            {
+                LastName = request.LastName,
+                FirstName = request.FirstName,
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PhoneNumber = request.PhoneNumber,
+                Adress = request.Adress,
+                IsAdmin = request.IsAdmin,
+            };
+
+            // Save the user to the database
+            _db.Users.Add(user);
+            _db.SaveChanges();
+
+            return Ok("User added successfully");
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(LoginRequest loginRequest)
+        {
+            // Validate login request
+            if (loginRequest == null || string.IsNullOrEmpty(loginRequest.Email) || string.IsNullOrEmpty(loginRequest.Password))
+            {
+                return BadRequest("Invalid login request");
+            }
+
+            // Find the user by email
+            var user = _db.Users.FirstOrDefault(u => u.Email == loginRequest.Email);
+
+            // Check if the user exists and verify the password
+            if (user == null || !BCrypt.Net.BCrypt.Verify(loginRequest.Password, user.PasswordHash))
+            {
+                return Unauthorized("Invalid email or password");
+            }
+
+            // Generate JWT token
+            var token = GenerateTokenString(user);
+
+            // Set the token in a cookie
+            Response.Cookies.Append("JwtToken", token, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true, 
+                SameSite = SameSiteMode.None, 
+                Expires = DateTime.UtcNow.AddHours(1), 
+            });
+
+            return Ok("Login successful");
+        }
+
+        // Validate email format
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public string GenerateTokenString(User user)
+        {
+            var claims = new List<Claim>
+            {
+               new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User"),
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("Jwt:Key").Value));
+
+            var signingCred = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var securityToken = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: signingCred);
+
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+            return tokenString;
+        }
+    }
+   
+    
+
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using ProjetNET.Controllers;
+using ProjetNET.Models;
 using ProjetNET.Services;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,16 +16,36 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddHostedService<DataSeeder>();
-//builder.Services.AddHostedService<DataSeeder2>();
+// builder.Services.AddHostedService<DataSeeder>();
+// builder.Services.AddHostedService<DataSeeder2>();
 
-
+// Add Identity services
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Add authentication and authorization
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        RequireExpirationTime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value))
+
+    };
+}
+           );
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,6 +56,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Add authentication and authorization middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
