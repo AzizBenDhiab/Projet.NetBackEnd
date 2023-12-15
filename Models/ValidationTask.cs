6ef1765af2031676db17eebccfec5c1b76f0e15b
anasneji2002 <nejianas2002@gmail.com>
2023-12-15 19:55:55 +0100

    adding anonymbox features

---
+++ Models/ValidationTask.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetNET.Models
+{
+    public class ValidationTask
+    {
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(Task))]
+        public Guid TaskId { get; set; }
+        public bool Validation { get; set; }
+        public String? Cause { get; set; }
+    }
+}
