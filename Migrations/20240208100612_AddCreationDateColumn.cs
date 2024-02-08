9160c387b87e23c6413435d3be787da247669f2f
rezgu <rezgui012@gmail.com>
2024-02-08 11:14:48 +0100

    added date column to table Blames

---
+++ Migrations/20240208100612_AddCreationDateColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreationDateColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "Blames",
+                nullable: true,
+                defaultValueSql: "GETDATE()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+    name: "CreationDate",
+    table: "Blames");
+        }
+    }
+}
